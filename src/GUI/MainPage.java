7a2187dd909a25f2c9c8442c7040f4a8b94b664d
Veltays <yassine.gran@student.hepl.be>
2025-04-16 00:30:18 +0200

    Projet Java FirstPush début UI encore aucune classe

---
+++ src/GUI/MainPage.java
@@ -0,0 +1,58 @@
+package GUI;
+
+import javax.swing.*;
+
+
+public class MainPage extends JFrame {
+    private JPanel MainWindow;    // Le panel transparent
+    private JTable tablePlaylist;
+    private JTable TablePlaylist;
+    private JPanel InfoBouton;
+    private JPanel Musique;
+    private JPanel ListePlaylist;
+    private JPanel ProfilPlaylist;
+    private JPanel PlaylistNamePanel;
+    private JPanel ButtonNamePanel;
+    private JButton Ajouter;
+    private JButton Supprimer;
+    private JButton Modifier;
+    private JLabel PhotoProrfil;
+    private JButton Ajouter1;
+    private JButton Supprimer1;
+    private JButton Modifier1;
+    private JScrollBar scrollBar1;
+    private JScrollBar scrollBar2;
+
+
+    public MainPage() {
+        setTitle("DiskHouse ");
+        setContentPane(MainWindow); //  on définit bien Fond comme conteneur
+        setSize(1200, 700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        setVisible(true);
+
+
+        styliserBouton(Ajouter);
+        styliserBouton(Supprimer);
+        styliserBouton(Modifier);
+
+        styliserBouton(Ajouter1);
+        styliserBouton(Supprimer1);
+        styliserBouton(Modifier1);
+
+    }
+    private void styliserBouton(JButton bouton) {
+        bouton.setContentAreaFilled(false);   // Enlève le fond
+        bouton.setBorderPainted(false);       // Enlève le contour
+        bouton.setFocusPainted(false);        // Enlève le focus (le carré)
+        bouton.setOpaque(false);              // Rend transparent
+    }
+
+
+
+    public static void main(String[] args) {
+        new MainPage();
+    }
+}
