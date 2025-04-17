09bc3ce8612dfe1a02de482002fd5c50a33698c9
Zach <zaertmovie@gmail.com>
2025-04-17 20:38:54 +0100

    Login creation

--- src/main/java/DiskHouse/view/view/Login.java
+++ src/main/java/DiskHouse/view/view/Login.java
@@ -1,4 +1,32 @@
 package DiskHouse.view.view;
 
-public class Login {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+
+public class Login extends JFrame {
+
+    private JPanel fond;
+    private JLabel logo;
+    private JLabel logintexte;
+    private JTextField textfieldusername;
+    private JPasswordField textfieldpassword;
+
+    public Login() {
+        setTitle("Login");
+        setSize(500, 450);
+        setContentPane(fond); //  on définit bien Fond comme conteneur
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        setVisible(true);
+
+
+
+    }
+
+    public static void main(String[] args) {
+        new Login(); // au lieu de MainPage
+    }
 }
--- src/main/java/DiskHouse/view/view/Register.java
+++ src/main/java/DiskHouse/view/view/Register.java
@@ -1,38 +1,7 @@
 package DiskHouse.view.view;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
 
-public class Register extends JFrame {
-
-    private JPanel Fond;
-    private JPanel Logo;
-    private JPanel TexteRegister;
-    private JTextField TextFieldUsername;
-    private JPanel ChampUsername;
-    private JTextField passwordField;
-    private JTextField passwordFieldConfirmPassword;
-    private JPanel ChampPassword;
-    private JPanel ChampConfirmPassword;
-    private JLabel LogoLabel;
-    private JLabel LabelTexteRegister;
-
-    public Register() {
-        setTitle("Registre");
-        setSize(500, 450);
-        setContentPane(Fond); //  on définit bien Fond comme conteneur
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-
-        setVisible(true);
-
-
-
-    }
-
-    public static void main(String[] args) {
-        new Register(); // au lieu de MainPage
-    }
+public class register {
+    private JPanel panel1;
 }
